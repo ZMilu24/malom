ac6ae797bd3e02d7e10687da9899cd1185eac241
ZMilan24 <93102357+ZMilu24@users.noreply.github.com>
2025-10-01 13:28:21 +0200

    Add files via upload

--- dot.py
+++ dot.py
@@ -2,6 +2,7 @@ class Dot():
     def __init__(self, pos, canvas): # pos = [x,y], tonari = [ Dot args]
         self.pos = pos
         self.dot = canvas.create_oval(pos[0]-10,pos[1]-10,pos[0]+10,pos[1]+10, fill="brown")
+        self.canvas = canvas
         pass
 
     def setNext(self, tonari):
--- game.py
+++ game.py
@@ -18,22 +18,64 @@ class GameScreen():
         self.mainText()
 
         innerLayer = [
-                            [Dot([200,200], self.canvas), Dot([250,200], self.canvas), Dot([300,200], self.canvas)],
+                            Dot([200,200], self.canvas), Dot([250,200], self.canvas), Dot([300,200], self.canvas),
                             Dot([200,250], self.canvas),                                Dot([300,250], self.canvas),
-                            [Dot([200,300], self.canvas), Dot([250,300], self.canvas), Dot([300,300], self.canvas)],
+                            Dot([200,300], self.canvas), Dot([250,300], self.canvas), Dot([300,300], self.canvas),
         ]
         middleLayer = [
-                            [Dot([150,150], self.canvas), Dot([250,150], self.canvas), Dot([350,150], self.canvas)],
+                            Dot([150,150], self.canvas), Dot([250,150], self.canvas), Dot([350,150], self.canvas),
                             Dot([150,250], self.canvas),                                Dot([350,250], self.canvas),
-                            [Dot([150,350], self.canvas), Dot([250,350], self.canvas), Dot([350,350], self.canvas)],
+                            Dot([150,350], self.canvas), Dot([250,350], self.canvas), Dot([350,350], self.canvas),
         ]
         outherLayer = [
-                            [Dot([100,100], self.canvas), Dot([250,100], self.canvas), Dot([400,100], self.canvas)],
+                            Dot([100,100], self.canvas), Dot([250,100], self.canvas), Dot([400,100], self.canvas),
                             Dot([100,250], self.canvas),                                Dot([400,250], self.canvas),
-                            [Dot([100,400], self.canvas), Dot([250,400], self.canvas), Dot([400,400], self.canvas)],
+                            Dot([100,400], self.canvas), Dot([250,400], self.canvas), Dot([400,400], self.canvas),
         ]
 
         gameMap = [innerLayer, middleLayer, outherLayer]
+
+        for layer in range(len(gameMap)):
+            for i in range(len(gameMap[layer])):
+                nodes = []
+                #if (i != 0 or i != 7 or i != 3 or i != 4):
+                    #nodes.append(gameMap[layer][i-1])
+                    #nodes.append(gameMap[layer][i+1])
+                if (i == 0):
+                    nodes.append(gameMap[layer][1])
+                    nodes.append(gameMap[layer][3])
+                elif (i == 3):
+                    nodes.append(gameMap[layer][0])
+                    nodes.append(gameMap[layer][5])
+                    if (layer == 1):
+                        nodes.append(gameMap[layer+1][3])
+                        nodes.append(gameMap[layer-1][3])
+                elif (i == 4):
+                    nodes.append(gameMap[layer][2])
+                    nodes.append(gameMap[layer][7])
+                    if (layer == 1):
+                        nodes.append(gameMap[layer+1][4])
+                        nodes.append(gameMap[layer-1][4])
+                elif (i == 7):
+                    nodes.append(gameMap[layer][4])
+                    nodes.append(gameMap[layer][6])
+                elif (i == 2):
+                    nodes.append(gameMap[layer][1])
+                    nodes.append(gameMap[layer][4])
+                elif (i == 5):
+                    nodes.append(gameMap[layer][6])
+                    nodes.append(gameMap[layer][3])
+                else:
+                    nodes.append(gameMap[layer][i+1])
+                    nodes.append(gameMap[layer][i-1])
+                    if (layer == 1):
+                        if (i == 1):
+                            nodes.append(gameMap[layer+1][1])
+                            nodes.append(gameMap[layer-1][1])
+                        elif (i == 6):
+                            nodes.append(gameMap[layer+1][6])
+                            nodes.append(gameMap[layer-1][6])
+                gameMap[layer][i].setNext(nodes)
 
         self.canvas.pack()
         self.root.mainloop()
