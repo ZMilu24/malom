a3fb99b9adf39cb0c90d15a0b73832de94c4e589
ZMilan24 <93102357+ZMilu24@users.noreply.github.com>
2025-10-01 10:53:32 +0200

    Update dot.py

--- dot.py
+++ dot.py
@@ -1,12 +1,12 @@
 class Dot():
-    def __init__(self, pos, canvas): # pos = [x,y], next = [ Dot args]
+    def __init__(self, pos, canvas): # pos = [x,y], tonari = [ Dot args]
         self.pos = pos
         self.dot = canvas.create_oval(pos[0]-10,pos[1]-10,pos[0]+10,pos[1]+10, fill="brown")
         pass
 
-    def setNext(self, next):
-        self.next = next
-        for i in next:
+    def setNext(self, tonari):
+        self.tonari = tonari
+        for i in tonari:
             if (i.pos != self.pos):
-                print("vonal rajzolás")
+                self.canvas.create_line(self.pos[0], self.pos[1], i.pos[0], i.pos[1])
         
